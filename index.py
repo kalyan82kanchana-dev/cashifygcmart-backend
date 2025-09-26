2e9171f876ae43630f938d123c34c9ddb9320a9b
kalyan82kanchana-dev <kalyan82kanchana@gmail.com>
2025-09-26 12:45:42 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,9 @@
+# Railway.app entry point
+from server import app
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
