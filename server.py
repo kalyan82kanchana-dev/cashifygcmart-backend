7bb02dedc7c3efc8b0850d63645d410581ba1b1f
kalyan82kanchana-dev <kalyan82kanchana@gmail.com>
2025-09-26 12:14:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -68,7 +68,7 @@ class GiftCardSubmission(BaseModel):
     zelleDetails: Optional[str] = ""
     cashAppTag: Optional[str] = ""
     btcAddress: Optional[str] = ""
-    chimeDetails: Optional[str] = 
+    chimeDetails: Optional[str] = ""
   ""# Email Template Functions
 def generate_confirmation_email_html(customer_name, reference_number):
     return f"""
