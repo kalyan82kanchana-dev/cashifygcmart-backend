7998f9cfef732bd1ede7d71c79d7d860c7698ab2
kalyan82kanchana-dev <kalyan82kanchana@gmail.com>
2025-09-26 13:17:16 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 from datetime import datetime
 import random
-import httpx
+# import httpx
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
